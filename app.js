079ce0919e8869a4bb629a795ee8a65c7388d666
briantmorr <briantmorr@gmail.com>
2018-03-29 20:00:04 -0700

    startup files

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3089;
+app.use(bodyParser.urlencoded({extended: true}));
+app.get('/', (req, res) => {res.status(200).send('Hello World'); });
+app.listen(port, ()=> {
+    console.log('Listening on port ' + port);
+});
+app.post('/hello', (req, res, next)=> {
+    var userName = req.body.user_name;
+    var botPayload = {
+        text: 'Hello' + username + ', Bark Bark Bark'
+    };
+    if (userName != 'slackbot'){
+        return res.status(200).json(botPayload);
+    } else {
+        return res.status(200).end();
+    }
+});
