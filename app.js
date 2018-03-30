3d7122a5a61881d8b7f13778d68f076459768bf8
Parker Rebensdorf <parker.rebensdorf@gmail.com>
2018-03-29 21:42:50 -0700

    added post req

--- app.js
+++ app.js
@@ -7,14 +7,26 @@ app.get('/', (req, res) => {res.status(200).send('Hello World Bark'); });
 app.listen(port, ()=> {
     console.log('Listening on port ' + port);
 });
-app.post('/hello', (req, res, next)=> {
-    var userName = req.body.user_name;
-    var botPayload = {
-        text: 'Hello' + username + ', Bark Bark Bark'
-    };
-    if (userName != 'slackbot'){
-        return res.status(200).json(botPayload);
-    } else {
-        return res.status(200).end();
-    }
+app.post('/bark', (req, res, next)=> {
+    res.send({
+        "ok": true,
+        "channel": "C9YPC1LMB",
+        "ts": "1522383631.000070",
+        "message": {
+            "type": "message",
+            "user": "U9YCMQNS0",
+            "text": "Bark",
+            "bot_id": "B9XV6AUC8",
+            "ts": "1522383631.000070"
+        }
+    });
+    // var userName = req.body.user_name;
+    // var botPayload = {
+    //     text: 'Hello' + username + ', Bark Bark Bark'
+    // };
+    // if (userName != 'slackbot'){
+    //     return res.status(200).json(botPayload);
+    // } else {
+    //     return res.status(200).end();
+    // }
 });
