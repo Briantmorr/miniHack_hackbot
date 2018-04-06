84b1d993a51e1fc3535f5d55237442826adf5435
briantmorr <briantmorr@gmail.com>
2018-04-05 20:17:51 -0700

    fixed message

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3089;
+
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.listen(port, ()=> {
     console.log('Listening on port ' + port);
 });
@@ -12,7 +14,7 @@ app.get('/', (req, res) => {res.status(200).send('Hello Matt'); });
 app.post('/hello', (req, res, next)=> {
     var userName = req.body.user_name;
     var botPayload = {
-        text: 'Hello' + username + ', Bark Bark Bark'
+        text: 'Hello' + userName + ', Bark Bark Bark'
     };
     if (userName != 'slackbot'){
         return res.status(200).json(botPayload);
